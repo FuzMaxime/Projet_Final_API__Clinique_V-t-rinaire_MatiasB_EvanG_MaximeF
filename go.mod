fe2f8c8d785411115373af5f5bb9ef55e1fc8fc4
fmaxime <maxime.fuzeau@ynov.com>
2024-11-14 15:37:57 +0100

    :tada: | arborescence

---
+++ go.mod
@@ -0,0 +1,5 @@
+module vet-clinic-api
+
+go 1.23.2
+
+require github.com/go-chi/chi/v5 v5.1.0
---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"vet-clinic-api/config"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func Routes(configuration *config.Config) *chi.Mux {
+    router := chi.NewRouter()
+    router.Mount("/api/v1/agecalculator", agecalculator.Routes(configuration))
+    return router
+}
+
+func main() {
+    // Initialisation de la configuration
+    configuration, err := config.New()
+    if err != nil {
+        log.Panicln("Configuration error:", err)
+    }
+
+    // Initialisation des routes
+    router := Routes(configuration)
+
+    log.Println("Serving on :8080")
+    log.Fatal(http.ListenAndServe(":8080", router))
+}
